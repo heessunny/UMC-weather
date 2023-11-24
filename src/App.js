15cd022312cffcbd40025d14e33a84de08a6e63c
heesun0419 <80321582+heesun0419@users.noreply.github.com>
2023-11-24 18:20:31 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+function App() {
+    const [location, setLocation]=useState('');
+    const API_KEY="2d447dd2a949c9aec177f2a67c5fe481";
+    const [result,setResult]=useState({});
+ 
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+ 
+
+
+    const searchWeather =async(e)=>{
+        if(e.key ==='Enter'){
+            try{
+                const data=await axios({
+                    method:'get',
+                    url: url
+                })
+console.log(data);
+setResult(data);
+            }
+            catch(err){
+                alert("해당 도시에 대한 정보가 없습니다");
+
+            }
+        }
+
+    }
+    return (
+    <AppWrap>
+        <div className='appContentWrap'>
+            <input 
+            placeholder='도시를 입력하세요'
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            type="text"
+            onKeyDown={searchWeather}
+            />
+            {
+                Object.keys(result).length!==0&&(  
+                <ResultWrap>
+                    <div calssName='city'>{result.data.name}</div>
+                    <div className="temperature">
+                        {Math.round(((result.data.main.temp-273.15)*10))/10}</div>
+                    <div className="sky">{result.data.weather[0].main}</div>
+                </ResultWrap>
+    )}
+
+        </div>
+
+
+    </AppWrap>
+  );
+}
+
+export default App;
+
+
+const AppWrap=styled.div`
+width: 100vw;
+height: 100vh;
+
+
+.appContentWrap{
+    left:50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    padding: 20px;
+}
+input{
+    padding: 16px;
+    border: 2px black solid;
+    border-radius: 16px;
+}
+`;
+const ResultWrap=styled.div`
+margin-top: 60px;
+padding:10px;
+border: 1px black solid;
+border-radius: 8px;
+.city{
+    font-size: 24px;
+}
+.temperature{
+    font-size: 60px;
+    margin-top: 8px;
+}
+.sky{
+    font-size: 20px;
+    text-align: right;
+    margin-top: 8px;
+}
+`;
